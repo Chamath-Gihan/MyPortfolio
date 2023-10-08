e3401d56e3bad99851b7099af195e4eb46804068
Chamath-Gihan <spchandanipushpakumari@gmail.com>
2023-10-08 17:13:07 +0530

    Knight rider assignment

--- assignments/JS/Knight/assets/JS/knight-script.js
+++ assignments/JS/Knight/assets/JS/knight-script.js
@@ -1,6 +1,14 @@
-const colorBoxes = document.querySelectorAll('.colour-box div');
+const colorBoxes = document.querySelectorAll('.colour-box div div');
 const btnStart = document.getElementById('button-start');
 const btnStop = document.getElementById('button-stop');
 const rangeInput = document.getElementById('range');
 const speedText = document.getElementById('speed');
 const waitingTimeText = document.getElementById('waiting-time');
+const audio = new Audio('assets/knightRider-music/knightrider.mp3');
+
+let interval;
+let animateTime = 50;
+let waitTime = 2000;
+let isAnimating = false;
+let direction = 1;
+let currentIndex = 0;
