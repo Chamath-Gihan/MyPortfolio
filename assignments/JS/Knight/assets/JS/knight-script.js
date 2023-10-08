66f70e0519a8c50a784e67d06372e9763979ef89
Chamath-Gihan <spchandanipushpakumari@gmail.com>
2023-10-08 14:50:24 +0530

    Knight rider assignment

---
+++ assignments/JS/Knight/assets/JS/knight-script.js
@@ -0,0 +1,6 @@
+const colorBoxes = document.querySelectorAll('.colour-box div');
+const btnStart = document.getElementById('button-start');
+const btnStop = document.getElementById('button-stop');
+const rangeInput = document.getElementById('range');
+const speedText = document.getElementById('speed');
+const waitingTimeText = document.getElementById('waiting-time');
