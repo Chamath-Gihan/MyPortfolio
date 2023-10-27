3fba582497c31b21896b87d89a1c48e2823eb9c4
Chamath-Gihan <spchandanipushpakumari@gmail.com>
2023-10-27 22:10:54 +0530

    customer form

--- assignments/JS/InMemoryPOS/controllers/validations/customerFormValidation.js
+++ assignments/JS/InMemoryPOS/controllers/validations/customerFormValidation.js
@@ -1,20 +1,42 @@
-document.getElementById("btnSaveCustomer").addEventListener("click", function(event) {
-    var customerId = document.getElementById("customerId").value;
-    var customerName = document.getElementById("customerName").value;
-    var customerAddress = document.getElementById("customerAddress").value;
-    var customerPhone = document.getElementById("customerPhone").value;
+$(document).ready(function() {
+    $("#btnSaveCustomer").click(function(event) {
+        // Prevent the form from submitting
+        event.preventDefault();
 
-    // Basic form validation
-    if (customerId === "" || customerName === "" || customerAddress === "" || customerPhone === "") {
-        alert("All fields are required");
-        event.preventDefault();
-    } else if (!isValidPhoneNumber(customerPhone)) {
-        alert("Invalid phone number");
-        event.preventDefault();
+        // Get form values
+        var customerId = $("#customerId").val();
+        var customerName = $("#customerName").val();
+        var customerAddress = $("#customerAddress").val();
+        var customerSalary = parseFloat($("#customerSalary").val()) || 0;
+
+        // Check if any field is empty
+        if (customerId === "" || customerName === "" || customerAddress === "" || customerSalary === 0) {
+            // Apply red border effect to empty fields for 3 seconds
+            $("input").each(function() {
+                if ($(this).val() === "") {
+                    $(this).css("border", "2px solid red");
+                }
+            });
+
+            // Reset border and show notification after 3 seconds
+            setTimeout(function() {
+                $("input").css("border", "");
+                showNotification("All fields are required");
+            }, 3000);
+        } else {
+            // Fields are not empty, no action performed
+        }
+    });
+
+    function showNotification(message) {
+        // Display notification
+        var notification = $("<div class='notification'>" + message + "</div>").appendTo("body");
+
+        // Automatically close the notification after 5 seconds
+        setTimeout(function() {
+            notification.fadeOut(300, function() {
+                $(this).remove();
+            });
+        }, 5000);
     }
 });
-
-function isValidPhoneNumber(phoneNumber) {
-    var phoneRegex = /^\d{10}$/;
-    return phoneRegex.test(phoneNumber);
-}
